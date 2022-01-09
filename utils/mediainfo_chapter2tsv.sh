4c5469eed1343c913cd987bf0b9350cd45362518
mba <mba@mbas-MacBook-Air.local>
2022-01-10 03:29:03 +0800

    .

--- utils/mediainfo_chapter2tsv.sh
+++ utils/mediainfo_chapter2tsv.sh
@@ -1,3 +1,3 @@
 clipname="${1%.*}"
-mediainfo $1 |grep ": CHAPTER" | sed -n    's/\./,/;N;h;s/\n/\t/;p;x;D;' | awk -v OFS='\t' '{print "EDL",$1,$4,'"\"| ${clipname} |\""',$3}'
+mediainfo $1 |grep ": CHAPTER" | sed -n    'N;s/\./,/g;h;s/\n/\t/;p;x;D;' | awk -v OFS='\t' '{print "EDL",$1,$4,'"\"| ${clipname} |\""',$3}'
 
