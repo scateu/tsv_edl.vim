5a698701f4008defbd9a4b14bcf9c4eaacd4d128
K <mbp13-touchbar@example.com>
2022-01-14 11:43:48 +0800

    Fri Jan 14 11:43:48 CST 2022

--- utils/scenecut_preview.sh
+++ utils/scenecut_preview.sh
@@ -14,10 +14,17 @@ secs2timecode() {
 	done
 }
 
-echo "writing to ${media%.*}_scenecut.tsv"
-#ffmpeg -y -i "./${media}" -vf "select=gt(scene\,0.4),showinfo,scale=160:-1,tile=6x80" -frames:v 1 -qscale:v 3 "${preview}" 2>&1 |  sed -n -r -l '/^.*showinfo.*pts_time.*$/ {s/.*pts_time:([0-9]+\.?[0-9]+).*/\1/g;p;}'  |  secs2timecode |  sed -n -l 'N;s/\./,/g;h;s/\n/\t/;p;x;D;' | sed -l 's/^/EDL\t/' | sed -l "s/$/\t| ${media%.*} |\t/" | tee "${media%.*}_scenecut.tsv"
-ffmpeg -y -i "./${media}" -vf "select=gt(scene\,0.1),showinfo" -f null - 2>&1 |  sed -n -r -l '/^.*showinfo.*pts_time.*$/ {s/.*pts_time:([0-9]+\.?[0-9]+).*/\1/g;p;}'  |  secs2timecode |  sed -n -l 'N;s/\./,/g;h;s/\n/\t/;p;x;D;' | sed -l 's/^/EDL\t/' | sed -l "s/$/\t| ${media%.*} |\t/" | tee "${media%.*}_scenecut.tsv"
+echo "Writing to ${media%.*}_scenecut.tsv"
 
+#ffmpeg -y -i "./${media}" -vf "select=gt(scene\,0.4),showinfo,scale=160:-1,tile=6x80" -frames:v 1 -qscale:v 3 "${preview}" 2>&1 |  sed -n -r -l '/^.*showinfo.*pts_time.*$/ {s/.*pts_time:([0-9]+\.?[0-9]+).*/\1/g;p;}'  |  secs2timecode |  sed -n -l 'N;s/\./,/g;h;s/\n/\t/;p;x;D;' | sed -l 's/^/EDL\t/' | sed -l "s/$/\t| ${media%.*} |\t/" | tee "${media%.*}_scenecut.tsv"
+#ffmpeg -y -i "./${media}" -vf "select=gt(scene\,0.1),showinfo" -f null - 2>&1 |  sed -n -r -l '/^.*showinfo.*pts_time.*$/ {s/.*pts_time:([0-9]+\.?[0-9]+).*/\1/g;p;}'  |  secs2timecode |  sed -n -l 'N;s/\./,/g;h;s/\n/\t/;p;x;D;' | sed -l 's/^/EDL\t/' | sed -l "s/$/\t| ${media%.*} |\t/" | tee "${media%.*}_scenecut.tsv"
+
+dirname="${media%.*}_scenecut_images"
+mkdir "${dirname}"
+echo "Will extract key frames into dir: ${dirname}/"
+echo "You may convert * slides.pdf afterwards"
+
+ffmpeg -y -i "./${media}" -vf "select=gt(scene\,0.1),showinfo"  -vsync vfr "${dirname}/${media%.*}-%05d.png" 2>&1 |  sed -n -r -l '/^.*showinfo.*pts_time.*$/ {s/.*pts_time:([0-9]+\.?[0-9]+).*/\1/g;p;}'  |  secs2timecode |  sed -n -l 'N;s/\./,/g;h;s/\n/\t/;p;x;D;' | sed -l 's/^/EDL\t/' | sed -l "s/$/\t| ${media%.*} |\t/" | tee "${media%.*}_scenecut.tsv"
 #threshold = 0.1 for webcast
 #threshold = 0.4 for movie
 #threshold is (0, 1)   the smaller the finer chopped
@@ -28,3 +35,9 @@ ffmpeg -y -i "./${media}" -vf "select=gt(scene\,0.1),showinfo" -f null - 2>&1 | 
 # https://stackoverflow.com/questions/12199631/convert-seconds-to-hours-minutes-seconds
 
 #ffmpeg -f lavfi -i "movie=${media},scdet=s=1:t=14" -vf "scale=160:-1,tile=6x85" -frames:v 1 -qscale:v 3 "${preview}"
+
+# or 
+#FFmpeg Scene selection : extracting iframes and detecting scene change - 2020  ☑
+#ffmpeg -i yosemiteA.mp4 -vf "select=gt(scene\,0.5), scale=640:360" -vsync vfr yosemiteThumb%03d.png
+#convert *.png -scale 640x360 a.pdf
+#cat *.png | ffmpeg -f image2pipe -i - -vf 'scale=128:72,tile=8x8' -an -vsync 0 scene/%03d.png
