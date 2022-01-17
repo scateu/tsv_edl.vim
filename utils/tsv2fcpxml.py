ea56029784d621cbee70b1a96445e0027abf4a5a
K <mbp13-touchbar@example.com>
2022-01-17 17:46:41 +0800

    Mon Jan 17 17:46:41 CST 2022

--- utils/tsv2fcpxml.py
+++ utils/tsv2fcpxml.py
@@ -8,6 +8,8 @@ import os
 import glob
 import sys
 
+GENERATE_SRT = True
+FCPX_SCALE = 90000
 video_formats = ['mkv', 'mp4', 'mov', 'mpeg', 'ts', 'avi']
 
 xmlheader1 = """<?xml version="1.0" encoding="UTF-8"?>
@@ -15,7 +17,7 @@ xmlheader1 = """<?xml version="1.0" encoding="UTF-8"?>
 
 <fcpxml version="1.8">
     <resources>
-        <format id="r1" frameDuration="3750/90000s" width="1920" height="1080" colorSpace="1-1-1 (Rec. 709)"/>
+        <format id="r1" frameDuration="3750/FCPX_SCALEs" width="1920" height="1080" colorSpace="1-1-1 (Rec. 709)"/>
 """
 xmlheader2 = """
         <asset id="{ref_id}" src="file://{mediapath}" start="0s" duration="6240s" hasVideo="1" hasAudio="1" format="r1" audioSources="1" audioChannels="1" audioRate="48000" />
@@ -25,7 +27,7 @@ xmlheader3 = """
     <library>
         <event name="DEFAULT">
             <project name="DEFAULT">
-                <sequence duration="15720000/90000s" format="r1" tcStart="0s" tcFormat="NDF" audioLayout="stereo" audioRate="48k">
+                <sequence duration="15720000/FCPX_SCALEs" format="r1" tcStart="0s" tcFormat="NDF" audioLayout="stereo" audioRate="48k">
                     <spine>
 """
 
@@ -46,13 +48,19 @@ xmltail = """                    </spine>
 <asset-clip name="MyMovie3" ref="r2" offset="5s" start="15s" duration="5s" audioRole="dialogue" /> 
 """
 
-def timecode_to_fcpx_time(timecode, fcp_scale=90000, FPS=24):
+def timecode_to_fcpx_time(timecode, fcp_scale=FCPX_SCALE, FPS=24):
     timecode = timecode.strip().replace(",", ":")
     h,m,s,ms = [int(d) for d in timecode.split(":")]
     # round to frames
-    ms_scaled = int(ms/1000.0 * FPS) * (fcp_scale / FPS)  #frames * 3750/90000 * 90000
+    ms_scaled = int(ms/1000.0 * FPS) * (fcp_scale / FPS)  #frames * 3750/FCPX_SCALE * FCPX_SCALE
     return h*3600*fcp_scale + m*60*fcp_scale + s*fcp_scale + ms_scaled
 
+def sec_to_srttime(sec):
+    HH = int(sec/3600.0)
+    MM = int((sec - 3600.0*HH)/60.0)
+    SS = int(sec - HH*3600.0 - MM*60.0)
+    MS = (sec - int(sec))*1000.0
+    return "%02d:%02d:%02d,%03d"%(HH,MM,SS,MS)
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -65,6 +73,8 @@ if __name__ == "__main__":
     xmlhead += xmlheader1
     offset = 0
     eprint("NOTE: 24FPS, 48000Hz. Bite me.")
+    srt_queue = []
+    srt_counter = 0
     while True:
         line = sys.stdin.readline()
         if not line:
@@ -72,7 +82,8 @@ if __name__ == "__main__":
         if line.startswith('EDL'):
             _l = line.strip()
             if _l.count('|'):
-                clipname = _l.split('|')[1].strip()
+                _items = _l.split('|')
+                clipname = _items[1].strip()
                 filenames_v = [ c for c in glob.glob("*%s*"%clipname) if os.path.splitext(c)[1][1:].lower() in video_formats ]
                 if len(filenames_v) > 0:
                     abspath = os.path.abspath(filenames_v[0])
@@ -83,15 +94,28 @@ if __name__ == "__main__":
                     media_assets[clipname] = [abspath, ref_id ] 
                 else:
                     ref_id = media_assets[clipname][1]
-                _l = _l.split('|')[0]
+
+                _r = _items[0].split() #['EDL', '01:26:16.12', '01:27:22.10']
+                fcpx_record_in = timecode_to_fcpx_time(_r[1])
+                fcpx_record_out  = timecode_to_fcpx_time(_r[2])
+                duration = fcpx_record_out - fcpx_record_in
+
+                if GENERATE_SRT:
+                    subtitle = _items[2].strip()
+                    if subtitle == "" or ("[ SPACE" in subtitle):
+                        pass
+                    else:
+                        srt_in = sec_to_srttime(offset / FCPX_SCALE)
+                        srt_out = sec_to_srttime((duration + offset) / FCPX_SCALE)
+                        srt_queue.append("%d"%srt_counter)
+                        srt_queue.append("%s --> %s"%(srt_in, srt_out))
+                        srt_queue.append("%s"%subtitle.replace("\\N", "\n"))
+                        srt_queue.append("\n")
+                        srt_counter += 1
             else:
                 continue
-            _l = _l.split() #['EDL', '01:26:16.12', '01:27:22.10']
-            record_in = timecode_to_fcpx_time(_l[1])
-            record_out  = timecode_to_fcpx_time(_l[2])
-            duration = record_out - record_in
-            xmlbody += "<asset-clip name=\"%s\" ref=\"%s\" offset=\"%d/90000s\" start=\"%d/90000s\" duration=\"%d/90000s\" audioRole=\"dialogue\" />\n"%(clipname, ref_id, offset, record_in, duration)
-            #xmlbody += "%s\t%s\t%s\t%s\t%s\n"%(clipname, ref_id, offset, record_in, duration) #DEBUG
+            xmlbody += "<asset-clip name=\"%s\" ref=\"%s\" offset=\"%d/90000s\" start=\"%d/90000s\" duration=\"%d/90000s\" audioRole=\"dialogue\" />\n"%(clipname, ref_id, offset, fcpx_record_in, duration) #FIXME 90000 -> FCPX_SCALE
+            #xmlbody += "%s\t%s\t%s\t%s\t%s\n"%(clipname, ref_id, offset, fcpx_record_in, duration) #DEBUG
             offset += duration 
     for k in media_assets:
         xmlhead += xmlheader2.format(ref_id=media_assets[k][1] , mediapath = urllib.parse.quote(media_assets[k][0]))
@@ -99,3 +123,10 @@ if __name__ == "__main__":
     print(xmlhead)
     print(xmlbody)
     print(xmltail)
+
+    srt_filename = "fcpxml.srt"
+    if GENERATE_SRT:
+        eprint("[srt] writing ",srt_filename)
+        with open(srt_filename, "w") as output_file:
+            output_file.write('\n'.join(srt_queue))
+
