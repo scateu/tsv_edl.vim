b963efd16bfb9ba681bb011833c3bd843d0eede3
k <k@mbp16m1.local>
2025-03-18 17:27:58 +0800

    rcutserver.bash for platypus

--- html/platypus/rcutserver.bash
+++ html/platypus/rcutserver.bash
@@ -1,5 +1,7 @@
 export LANG=en_US.UTF-8; export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin/; 
+echo cp rcut2.html "$1"/index.html
 cp rcut2.html "$1"/index.html
+
 RESOURCE_PWD="$(pwd)"
 echo APP RESOURCE PATH: "${RESOURCE_PWD}"
 cd "$1"
